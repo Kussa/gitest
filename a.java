a3c5cc43762fca2b147e8d9d17eceaf9e15e7e95
Kussa <kussa@qq.com>
2018-11-27 20:14:49 +0800

    test

--- a.java
+++ a.java
@@ -1,5 +1,5 @@
 
 public static final String EMAIL_SMTPHOSTNAME = "smtp.163.com";
-public static final String EMAIL_FROM = "guest@cheyipai";
+public static final String EMAIL_FROM = "guest@cheyipai.com";
 public static final String EMAIL_ACCOUNT = "guest";
 public static final String EMAIL_PASSWORD = "13113010112";
